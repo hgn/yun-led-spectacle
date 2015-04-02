b20c1d481ec7264a5ef2ef717591b23023cfa640
Hagen Paul Pfeifer <hagen@jauu.net>
2015-04-02 21:58:21 +0200

    add two new signals and one mode
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- yun-led-spectacle.ino
+++ yun-led-spectacle.ino
@@ -14,6 +14,16 @@
 
 CRGB leds[NUM_LEDS];
 
+#undef DEBUG
+
+#ifdef DEBUG
+#define DBG_PRINT(x, ...) Console.print(x, ##__VA_ARGS__)
+#define DBG_PRINTLN(x, ...) Console.println(x, ##__VA_ARGS__)
+#else
+#define DBG_PRINT(x, ...)
+#define DBG_PRINTLN(x, ...)
+#endif
+
 enum {
     TYPE_UNKNOWN = 0,
     TYPE_MODE = 1,
@@ -23,11 +33,13 @@ enum {
 enum {
     MODE_DISCO = 1,
     MODE_AMBIENT,
+    MODE_AMBIENT_DYNAMIC,
     MODE_EXP,
 };
 
 enum {
     SIGNAL_ALARM = 1,
+    SIGNAL_GREEN_PULSE,
 };
 
 #define DEFAULT_MODE MODE_AMBIENT
@@ -36,7 +48,7 @@ YunServer server;
 
 void setup_debug()
 {
-#if 0
+#ifdef DEBUG
     Console.begin();
 
     while (!Console) {
@@ -82,6 +94,8 @@ int signalCommand(YunClient client, int *what)
     String mode = client.readStringUntil('\r');
     if (mode == "alarm") {
         *what = SIGNAL_ALARM;
+    } else if (mode == "green-pulse") {
+        *what = SIGNAL_GREEN_PULSE;
     } else {
         // invalid, no default signal
         return TYPE_UNKNOWN;
@@ -97,6 +111,8 @@ int modeCommand(YunClient client, int *what)
         *what = MODE_DISCO;
     } else if (mode == "ambient") {
         *what = MODE_AMBIENT;
+    } else if (mode == "ambient-dynamic") {
+        *what = MODE_AMBIENT_DYNAMIC;
     } else if (mode == "exp") {
         *what = MODE_EXP;
     }
@@ -138,6 +154,25 @@ void signal_alarm(void)
     }
 }
 
+void signal_green_pulse(void)
+{
+    int loop_count = 3, i, j, k;
+
+    while (loop_count-- > 0) {
+
+        for (i = 0; i < NUM_LEDS; i++)
+            leds[i] = CRGB::Green;
+            FastLED.show(); delay(50);
+
+        for (j = 0; j < 16; j++) {
+          for (i = 0; i < NUM_LEDS; i++) {
+            leds[i].fadeLightBy(16);
+          }
+          FastLED.show(); delay(50);
+        }
+    }
+}
+
 void mode_ambient(void)
 {
     for (int i = 0; i < NUM_LEDS; i++) {
@@ -145,6 +180,28 @@ void mode_ambient(void)
     }
     FastLED.show();
     delay(500);
+}
+
+void mode_ambient_dynamic(void)
+{
+    int wait = 150;
+    int i, c, color_start = 32, color_end = 40;
+
+    for (c = color_start; c < color_end; c++) {
+        for(i = 0; i < NUM_LEDS; i++) {
+            leds[i].setHSV(c, 255, 255);
+        }
+        FastLED.show();
+        delay(wait);
+    }
+
+    for (c = color_end; c > color_start; c--) {
+        for(i = 0; i < NUM_LEDS; i++) {
+            leds[i].setHSV(c, 255, 255);
+        }
+        FastLED.show();
+        delay(wait);
+    }
 }
 
 void mode_exp(void)
@@ -165,7 +222,10 @@ void handle_signal(int signal_no)
             Console.println("signal alarm");
             signal_alarm();
             break;
-
+        case SIGNAL_GREEN_PULSE:
+            Console.println("signal green-pulse");
+            signal_green_pulse();
+            break;
     };
 }
 
@@ -204,10 +264,13 @@ void loop()
             Console.println("mode ambient");
             mode_ambient();
             break;
+        case MODE_AMBIENT_DYNAMIC:
+            Console.println("mode ambient-dynamic");
+            mode_ambient_dynamic();
+            break;
         case MODE_EXP:
             Console.println("mode exp");
             mode_exp();
             break;
     }
-
 }
